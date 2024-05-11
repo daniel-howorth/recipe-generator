aca3ce62a0779c220479a8e371ac9d80fa8a1062
Daniel Howorth <daniel.howorth@sky.com>
2024-05-12 00:17:39 +0100

    initial recipe search form layout

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+const baseURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com";
 
+const url = `${baseURL}/recipes/complexSearch?instructionsRequired=true&addRecipeInformation=true&offset=${Math.floor(
+  Math.random() * 900
+)}&number=1`;
+
+const options = {
+  method: "GET",
+  headers: {
+    "x-rapidapi-key": "5ade7491ccmsh53c0bf4987bab29p1d39bajsnda79064f7111",
+    "x-rapidapi-host": "spoonacular-recipe-food-nutrition-v1.p.rapidapi.com",
+    "Content-Type": "application/json",
+  },
+};
+
+const getRecipes = async () => {
+  try {
+    const response = await fetch(url, options);
+    const results = await response.json();
+    console.log(results);
+  } catch {
+    console.log("there was an error");
+  }
+};
+
+// getRecipes();
