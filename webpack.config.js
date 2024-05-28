c306ff92af254d2e2c209b868ae5993538e3bb9e
Daniel Howorth <daniel.howorth@sky.com>
2024-05-28 19:05:56 +0100

    configured webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,20 @@
-// const path = require("path");
+const path = require("path");
 
-// module.exports = {
-//   // The entry point file
-//   entry: "./src/index.js",
-//   // The location of the build folder
-//   output: {
-//     path: path.resolve(__dirname, "dist"),
-//     filename: "bundle.js",
-//   },
-//   // Optional and for development only. This provides the ability to
-//   // map the built code back to the original source format when debugging.
-//   devtool: "eval-source-map",
-// };
+module.exports = {
+  // entry points
+  entry: {
+    "search-recipes": "./src/js/search-recipes.js",
+    "analyse-recipes": "./src/js/analyse-recipes.js",
+    "calculate-macros": "./src/js/calculate-macros.js",
+  },
+  // The location of the build folder
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].bundle.js",
+  },
+  // Optional and for development only. This provides the ability to
+  // map the built code back to the original source format when debugging.
+  devtool: "eval-source-map",
+  watch: true,
+  mode: "development",
+};
