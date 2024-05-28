3c6f70a9965c3ddf291e5327c434e1041d5e0e65
Daniel Howorth <daniel.howorth@sky.com>
2024-05-28 18:38:32 +0100

    restructured app file structure

---
+++ src/js/index.js
@@ -0,0 +1,14 @@
+// import { initializeApp } from "firebase/app";
+
+// // Firebase configuration
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC44pe49fNHZPEFUzHFMcIKNidCGLIwEAw",
+//   authDomain: "recipe-generator-c1fdb.firebaseapp.com",
+//   projectId: "recipe-generator-c1fdb",
+//   storageBucket: "recipe-generator-c1fdb.appspot.com",
+//   messagingSenderId: "824828698274",
+//   appId: "1:824828698274:web:d676710052aaccba1f41fb",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
