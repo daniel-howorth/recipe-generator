364a8fcfb4c577a63f95740488aee8e77788d44a
Daniel Howorth <daniel@Daniels-MacBook-Pro.local>
2024-05-30 11:25:18 +0100

    moved firebaseui initialisation to auth

--- src/js/login.js
+++ src/js/login.js
@@ -1,21 +1,5 @@
 import firebase from "./index.js";
-import "firebase/compat/auth";
-import * as firebaseui from "firebaseui";
-
-const uiConfig = {
-  signInSuccessUrl: "search-recipes.html",
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    {
-      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
-      requireDisplayName: false,
-    },
-    // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
-  ],
-};
-
-const ui = new firebaseui.auth.AuthUI(firebase.auth());
+import { ui, uiConfig } from "./auth.js";
 
 ui.start("#firebaseui-auth-container", uiConfig);
 
