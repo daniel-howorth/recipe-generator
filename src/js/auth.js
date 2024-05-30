615a1bf22de9936b672737c8bb99efd8b85c3bdb
Daniel Howorth <daniel.howorth@sky.com>
2024-05-30 11:25:18 +0100

    moved firebaseui initialisation to auth

--- src/js/auth.js
+++ src/js/auth.js
@@ -1,5 +1,21 @@
 import firebase from "./index.js";
 import "firebase/compat/auth";
+import * as firebaseui from "firebaseui";
+
+export const uiConfig = {
+  signInSuccessUrl: "search-recipes.html",
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD,
+      requireDisplayName: false,
+    },
+    // firebaseui.auth.AnonymousAuthProvider.PROVIDER_ID
+  ],
+};
+
+export const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
 export async function signOutUser() {
   try {
