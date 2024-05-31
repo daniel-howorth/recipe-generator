5744ef28290c81f9d38dc8c572fce93a3e526cbc
Daniel Howorth <daniel.howorth@sky.com>
2024-05-31 12:04:36 +0100

    moved recipe card build functions to recipe-processor module

--- src/js/search-recipes.js
+++ src/js/search-recipes.js
@@ -2,9 +2,9 @@ import { isNumber, validateInput } from "./utils.js";
 import firebase from "./index.js";
 import { signOutUser, getCurrentUserId } from "./auth.js";
 import { saveRecipe } from "./db.js";
+import { buildRecipeCard } from "./recipe-processor.js";
 
 const searchBtn = document.querySelector("button.search-btn");
-const recipeCard = document.querySelector(".recipe");
 
 let currentRecipeData = {};
 
@@ -29,7 +29,7 @@ searchBtn.addEventListener("click", async (event) => {
     const recipeData = await getRecipes(url, options);
     displayRecipe(recipeData);
     hideSearchForm();
-    saveRecipe(getCurrentUserId(), currentRecipeData);
+    // saveRecipe(getCurrentUserId(), currentRecipeData);
   }
 });
 
@@ -93,17 +93,21 @@ function hideSearchForm() {
 }
 
 function displayRecipe(data) {
-  document.querySelector(".recipe-wrapper").classList.remove("hidden");
+  const recipeWrapper = document.querySelector(".recipe-wrapper");
+  recipeWrapper.classList.remove("hidden");
 
   // clear any existing recipe data
-  while (recipeCard.firstChild) {
-    recipeCard.removeChild(recipeCard.firstChild);
+  while (recipeWrapper.firstChild) {
+    recipeWrapper.removeChild(recipeWrapper.firstChild);
   }
 
   // if no results, display message.
   if (!data?.results?.length) {
     const noResultsMsg = `<div class="centered-text"><span>Could not find any recipes. Try tweaking your requirements.</span></div>`;
+    const recipeCard = document.createElement("article");
+    recipeCard.setAttribute("class", "recipe card");
     recipeCard.innerHTML = noResultsMsg;
+    recipeWrapper.appendChild(recipeCard);
     return;
   }
 
@@ -157,157 +161,37 @@ function displayRecipe(data) {
   const instructionsList =
     data.results[0].analyzedInstructions?.[0]?.steps || [];
 
+  // currentRecipeData = {
+  //   id: data.results[0].id.toString(),
+  //   title: title,
+  //   image: image,
+  //   readyIn: readyIn,
+  //   servings: servings,
+  //   nutrients: [calories, protein, carbs, fat, fiber, sugar],
+  //   ingredientsList: ingredientsList,
+  //   instructionsList: instructionsList,
+  // };
+
   currentRecipeData = {
     id: data.results[0].id.toString(),
     title: title,
     image: image,
     readyIn: readyIn,
     servings: servings,
-    nutrients: [calories, protein, carbs, fat, fiber, sugar],
+    nutrients: {
+      calories: calories,
+      protein: protein,
+      carbs: carbs,
+      fat: fat,
+      fiber: fiber,
+      sugar: sugar,
+    },
     ingredientsList: ingredientsList,
     instructionsList: instructionsList,
   };
 
-  const recipeImgHTML = `
-    <img
-      src=${image}
-      alt="food"
-    />`;
-
-  // conditionally render 'ready in' if data exists
-  const recipeHeaderHTML = `
-    <h1>${title}</h1>
-    ${
-      readyIn
-        ? `<div class="ready-in">
-            <div class="visually-hidden">ready in</div>
-            <img src="../../assets/clock.svg" alt="clock" class="clock-icon" />
-            <span>${readyIn} mins</span>
-          </div>`
-        : ""
-    }`;
-
-  const nutritionalContentHTML = `
-    <header class="section-header">
-      <h2>Nutrition per serving</h2>
-      <button
-        class="toggle-content-btn"
-        data-toggle="nutritional-content"
-      >
-        <div class="visually-hidden">Toggle nutritional content.</div>
-        <img src="../../assets/chevron-up.svg" alt="chevron" />
-      </button>
-    </header>
-
-    <div id="nutritional-content">
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${calories}</div>
-        <span class="nutrition-stat-label">Calories</span>
-      </div>
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${protein}</div>
-        <span class="nutrition-stat-label">Protein</span>
-      </div>
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${carbs}</div>
-        <span class="nutrition-stat-label">Carbs</span>
-      </div>
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${fat}</div>
-        <span class="nutrition-stat-label">Fat</span>
-      </div>
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${fiber}</div>
-        <span class="nutrition-stat-label">Fiber</span>
-      </div>
-      <div class="nutrition-stat">
-        <div class="nutrition-stat-content">${sugar}</div>
-        <span class="nutrition-stat-label">Sugar</span>
-      </div>
-    </div>`;
-
-  let ingredientsSectionHTML = `
-    <header class="section-header">
-      <h2>Ingredients</h2>
-      <button
-        class="toggle-content-btn"
-        data-toggle="ingredients-content"
-      >
-        <div class="visually-hidden">Toggle instructions.</div>
-        <img src="../../assets/chevron-down.svg" alt="chevron" />
-      </button>
-    </header>
-    <div class="hidden" id="ingredients-content">
-      ${servings ? `<span>${servings} servings</span>` : ""}
-      <ul class="ingredients-list">
-  `;
-
-  // conditionally render ingredients if data exists
-  if (ingredientsList.length) {
-    for (let ingredient of ingredientsList) {
-      ingredientsSectionHTML += `<li>${
-        ingredient.original ? ingredient.original : "n/a"
-      }</li>`;
-    }
-  } else {
-    ingredientsSectionHTML += `<li>No ingredients found. What's in the fridge?</li>`;
-  }
-
-  ingredientsSectionHTML += `</ul></div>`;
-
-  let instructionsSectionHTML = `
-    <header class="section-header">
-      <h2>Instructions</h2>
-      <button
-        class="toggle-content-btn"
-        data-toggle="instructions-content"
-      >
-        <div class="visually-hidden">Toggle instructions.</div>
-        <img src="../../assets/chevron-down.svg" alt="chevron" />
-      </button>
-    </header>
-    <ol class="hidden" id="instructions-content">
-  `;
-
-  // conditionally render instructions if data exists
-  if (instructionsList.length) {
-    for (let instruction of instructionsList) {
-      instructionsSectionHTML += `<li>${
-        instruction.step ? instruction.step : "n/a"
-      }</li>`;
-    }
-  } else {
-    instructionsSectionHTML += `<li>No instructions found. Just wing it.</li>`;
-  }
-
-  instructionsSectionHTML += `</ol>`;
-
-  // build recipe card
-  const recipeImg = document.createElement("div");
-  recipeImg.classList.add("recipe-img-wrapper");
-  recipeImg.innerHTML = recipeImgHTML;
-
-  const recipeHeader = document.createElement("header");
-  recipeHeader.classList.add("recipe-header");
-  recipeHeader.innerHTML = recipeHeaderHTML;
-
-  const nutritionalContent = document.createElement("section");
-  nutritionalContent.classList.add("nutritional-content");
-  nutritionalContent.innerHTML = nutritionalContentHTML;
-
-  const ingredientsSection = document.createElement("section");
-  ingredientsSection.classList.add("ingredients");
-  ingredientsSection.innerHTML = ingredientsSectionHTML;
-
-  const instructionsSection = document.createElement("section");
-  instructionsSection.classList.add("instructions");
-  instructionsSection.innerHTML = instructionsSectionHTML;
-
-  recipeCard.appendChild(recipeImg);
-  recipeCard.appendChild(recipeHeader);
-  recipeCard.appendChild(nutritionalContent);
-  recipeCard.appendChild(ingredientsSection);
-  recipeCard.appendChild(instructionsSection);
+  const recipeCard = buildRecipeCard(currentRecipeData);
+  recipeWrapper.appendChild(recipeCard);
 
   applyToggleContentEventListeners();
 }
