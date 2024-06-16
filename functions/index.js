6d8cef929aa946ca47e6b245d1e2453bc7165701
Daniel Howorth <daniel.howorth@sky.com>
2024-06-16 13:09:26 +0100

    initialised firebase functions

---
+++ functions/index.js
@@ -0,0 +1,30 @@
+/**
+ * Import function triggers from their respective submodules:
+ *
+ * const {onCall} = require("firebase-functions/v2/https");
+ * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
+ *
+ * See a full list of supported triggers at https://firebase.google.com/docs/functions
+ */
+
+// const functions = require("firebase-functions");
+// const { onRequest } = require("firebase-functions/v2/https");
+// const logger = require("firebase-functions/logger");
+
+// const { initializeApp } = require("firebase-admin");
+
+// const axios = require("axios");
+
+// const apiKey = functions.config().api.key;
+
+// Create and deploy your first functions
+// https://firebase.google.com/docs/functions/get-started
+
+// exports.helloWorld = onRequest((request, response) => {
+//   logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+
+// initializeApp();
+
+// firebase functions:config:set api.key="your_secret_api_key"
