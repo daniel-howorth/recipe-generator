5d3bd1e259570d468dd16f259c86a8dfde32c124
Daniel Howorth <daniel.howorth@sky.com>
2024-06-18 22:10:38 +0100

    moved getRecipes to cloud function

---
+++ functions/getRecipeAnalysis.js
@@ -0,0 +1,6 @@
+// const { onRequest } = require("firebase-functions/v2/https");
+// // const logger = require("firebase-functions/logger");
+
+// const fetch = require("node-fetch");
+
+// const calorieninjasApiKey = functions.config().calorieninjasapi.apikey;
