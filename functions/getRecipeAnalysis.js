ff0602cfc87ecc658df5efd3beb1b67b31b98035
Daniel Howorth <daniel@Daniels-MacBook-Pro.local>
2024-06-19 14:55:27 +0100

    moved analyse recipe to cloud function

--- functions/getRecipeAnalysis.js
+++ functions/getRecipeAnalysis.js
@@ -1,6 +1,75 @@
-// const { onRequest } = require("firebase-functions/v2/https");
-// // const logger = require("firebase-functions/logger");
+/* eslint-disable */
 
-// const fetch = require("node-fetch");
+const functions = require("firebase-functions");
+const { onRequest } = require("firebase-functions/v2/https");
+const { defineSecret } = require("firebase-functions/params");
+const logger = require("firebase-functions/logger");
+const cors = require("cors")({ origin: true }); // change origin when app is deployed
+const fetch = require("node-fetch");
 
-// const calorieninjasApiKey = functions.config().calorieninjasapi.apikey;
+const baseURL = "https://api.calorieninjas.com/v1/";
+const options = {
+  method: "GET",
+  headers: {
+    "X-Api-Key": "",
+    "Content-Type": "application/json",
+  },
+};
+const calorieninjasApiKey = defineSecret("CALORIENINJAS_API_KEY");
+
+exports.getrecipeanalysis = onRequest(
+  { region: "europe-west2", cors: true, secrets: [calorieninjasApiKey] },
+  async (req, res) => {
+    if (req.method === "OPTIONS") {
+      res.set("Access-Control-Allow-Origin", "*");
+      res.set("Access-Control-Allow-Methods", "GET");
+      res.set("Access-Control-Allow-Headers", "Content-Type");
+      res.status(204).send("");
+      return;
+    }
+
+    cors(req, res, async () => {
+      let apiKey = calorieninjasApiKey.value();
+
+      if (!apiKey) {
+        logger.error("request attempted but API key not found");
+        res.status(500).send("500 Error: API key not found");
+      } else {
+        options.headers["X-Api-Key"] = apiKey;
+      }
+
+      let recipeQuery = req.query.recipeQuery;
+
+      if (!recipeQuery) {
+        res.status(400).send("400 Error: Missing query data");
+        return;
+      }
+
+      const url = buildUrl(recipeQuery);
+
+      try {
+        logger.log(`sending request: ${url}`);
+        const response = await fetch(url, options);
+        if (response.ok) {
+          const result = await response.json();
+          logger.log("request successful");
+          res.status(200).json(result);
+        } else {
+          const errorDetails = await response.json().catch(() => ({}));
+          throw new Error(
+            `request failed with status ${response.status}: ${JSON.stringify(
+              errorDetails
+            )}`
+          );
+        }
+      } catch (error) {
+        logger.error(error.message);
+        res.status(500).send(error.message);
+      }
+    });
+  }
+);
+
+function buildUrl(query) {
+  return baseURL + "nutrition?query=" + query;
+}
